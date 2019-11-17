1ca7d4d8caaf65111f75f182127905346a77448a
Gintire <jin3670@gmail.com>
2019-11-17 12:36:45 +0900

    Changed Dockerfile

--- src/test/java/com/example/gintire/JsonTest.java
+++ src/test/java/com/example/gintire/JsonTest.java
@@ -1,11 +1,9 @@
 package com.example.gintire;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -22,7 +20,7 @@ public class JsonTest {
     public void getJson_test() throws  Exception{
         mvc.perform(MockMvcRequestBuilders.get("/api/products"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("[{\"id\":1,\"name\":\"IPhone11\",\"desc\":\"Apple phone\",\"price\":1500000,\"image_path\":\"/images/iphone11\"},{\"id\":2,\"name\":\"IPhoneX\",\"desc\":\"Apple phone\",\"price\":1100000,\"image_path\":\"/images/iphoneX\"}]"))
+                .andExpect(MockMvcResultMatchers.content().json("[{\"id\":1,\"name\":\"IPhone11\",\"desc\":\"Apple phone\",\"price\":1500000,\"image_path\":\"/images/iphone11\"},{\"id\":2,\"name\":\"IPhoneX\",\"desc\":\"Apple phone\",\"price\":1100000,\"image_path\":\"/images/iphoneX\"}]"))
                 .andDo(MockMvcResultHandlers.print());
     }
 }
